35c162acf245a09ac74a4e732cf0cdbca1c7b625
Alvaro Bautista <alvarobp@gmail.com>
2011-01-24 00:25:35 +0100

    formatted_date helper, quite helpful to Spaniards

--- lib/facturae_print/renderers/html_renderer_helpers.rb
+++ lib/facturae_print/renderers/html_renderer_helpers.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module FacturaePrint
   module Renderers
     module HTMLRendererHelpers
@@ -20,6 +22,9 @@ module FacturaePrint
         [individual.name, individual.first_surname, individual.second_surname].compact.join(' ')
       end
       
+      def formatted_date(date, strftime="%d/%m/%Y")
+        (date.is_a?(String) ? Date.parse(date) : date).strftime(strftime)
+      end
     end
   end
 end
